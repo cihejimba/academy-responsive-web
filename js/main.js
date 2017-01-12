f99b867ca4a3002c946036f78df82eeedd954993
Jen Zajac <jen@catalyst.net.nz>
2017-01-12 17:52:51 +1300

    Basic starter files
    includes data.json as an alternative data source if Python API down

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+  el: '.content',
+  
+  data: {
+    movies: []
+  },
+  
+  created: function() {
+    this.fetchData();
+  },
+  
+  methods: {
+    fetchData: function() {
+      var self = this;
+      var list = '/api/';
+      
+      fetch(list).then(function(response) {
+        return response.json();
+      }).then(function(json){
+        self.movies = json.movies;
+      })
+    }
+  }
+})
